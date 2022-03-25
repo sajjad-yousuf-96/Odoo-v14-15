b6dde7f9e0dda1745a303a8fb528e4d92f35943e
MUHAMMAD SAJJAD <51717713+sajjad-yousuf-96@users.noreply.github.com>
2022-03-25 23:56:22 +0500

    25-march commit

---
+++ msy_whatsapp/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'MSY WHATSAPP Software',
+    'version' : '1.1',
+    'summary': 'MSY Whatsapp Software',
+    'sequence': -100,
+    'description': """MSY WHATSAPP SOFTWARE""",
+    'category': 'Accounting/Accounting',
+    'website': 'https://www.odoo.com/page/billing',
+    'depends' : [],
+    'data': ['views/sale.xml'],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
